c9eb3a7fc6bfb299c0e33ceacde48e14f479a49d
some <zubkoffalex4nder@yandex.ru>
2024-03-12 21:12:42 +0300

    fix links

--- src/index.php
+++ src/index.php
@@ -11,12 +11,12 @@
         <title>About</title>
         <link rel="stylesheet" href="/style/style.css">
     </head>
+   
+    
 
-   
-  
-   
     <body class="body blur">
-
+        
+        
         <div class="particles_container">
             <?php for ($i = 1; $i <= 100; $i++) { ?>   
                 <div class="circle-container">
@@ -25,28 +25,29 @@
             <?php } ?>
         </div>
 
-        <!-- <div class="test_base">
-            <div class="test"></div>
-        </div> -->
 
+        
         <div class="container">
-    
+
+            
+
             <div class="about_box"> 
                 <img class="circle-image" src="/assets/myface.jpg"/>
                 <h1 class="title">Some</h1>
-                <h1 class="txt">I'm just another creator who love make things</h1>
+                <h1 class="txt">Just another creator who love make things</h1>
                 
                 <div class="circle_links">
-                    <a href=""><img class="link-image" src="/assets/sketchfab.svg"></a>
-                    <a href=""><img class="link-image" src="/assets/steam.svg"></a>
-                    <a href=""><img class="link-image" src="/assets/youtube.svg"></a>
-                    <a href=""><img class="link-image" src="/assets/github.svg"></a>
+                    <a href="https://sketchfab.com/susanin.krumbelvelikolep"><img class="link-image" src="/assets/sketchfab.svg"></a>
+                    <a href="https://steamcommunity.com/profiles/76561198412172044/"><img class="link-image" src="/assets/steam.svg"></a>
+                    <a href="https://www.youtube.com/channel/UCY2EUY4h6knWxZG1n8UEjlQ"><img class="link-image" src="/assets/youtube.svg"></a>
+                    <a href="https://github.com/somethink000"><img class="link-image" src="/assets/github.svg"></a>
 
                 </div>
                 
             </div>
             
         </div>
-    </body>
+       
+    </body> 
 
 </html>
