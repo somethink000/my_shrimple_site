4a14bb1ccf40a2c90db9984b865f8328951afddc
some <zubkoffalex4nder@yandex.ru>
2024-03-12 11:55:07 +0300

    "init"

---
+++ src/index.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>none</title>
+        <link rel="stylesheet" href="/style/style.css">
+    </head>
+
+   
+  
+   
+    <body class="body blur">
+
+    
+        <?php for ($i = 1; $i <= 100; $i++) { ?>   
+            <div class="circle-container">
+                <div class="circle"></div>
+            </div>
+        <?php } ?>
+
+
+
+        <div class="container">
+    
+            <div class="about_box"> 
+                <img class="circle-image" src="/assets/myface.jpg"/>
+                <h1 class="title">Some</h1>
+                <h1 class="txt">I'm just another creator who love make things</h1>
+            
+                <a href=""></a>
+            </div>
+            
+        </div>
+    </body>
+
+</html>
